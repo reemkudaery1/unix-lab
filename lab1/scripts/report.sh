768b868b0cf6affcb2e1bb5c85fee4e60b29146c
reemkudaery1 <reemkudaery1@example.com>
2026-05-06 22:15:18 +0300

    Clean and correct lab1 structure

--- lab1/scripts/report.sh
+++ lab1/scripts/report.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-today=$(date +%Y-%m-%d)
-
-touch ~/shell_lab/notes/report_${today}.txt
-
-echo "User Name: $(whoami)" >> ~/shell_lab/notes/report_${today}.txt
-
-echo "Current Path: $(pwd)" >> ~/shell_lab/notes/report_${today}.txt
-
-file_count=$(ls -1 ~/shell_lab/data | wc -l)
-
-echo "Number of files in data: $file_count" >> ~/shell_lab/notes/report_${today}.txt
-
-echo "------------------------"
-echo "Success: Report Created"
-echo "------------------------"
+echo 'Lab 1 script'
